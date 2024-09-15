1f3e81e96f622f89f0612d0acfa873df46654156
Roy Lopez <ryhsu2@gmail.com>
2024-09-15 16:52:56 +0200

    Update package.json and package-lock.json to include tiktoken dependency

--- chat_app/src/tools/index.ts
+++ chat_app/src/tools/index.ts
@@ -1,10 +1,6 @@
 import OpenAI from "openai";
-import { encoding_for_model } from "tiktoken";
 
 const openai = new OpenAI();
-const encoder = encoding_for_model("gpt-4o-mini");
-
-const MAX_TOKENS = 16000;
 
 function getTime() {
   return new Date().toLocaleTimeString();
@@ -58,23 +54,6 @@ async function createChatCompletion() {
   });
 
   console.log(secondResponse.choices[0].message.content);
-
-  const responseRole = response.choices[0].message.role;
-  const responseMessage = response.choices[0].message.content;
-
-  context.push({
-    role: "assistant",
-    content: responseMessage,
-  });
-
-  if (
-    response.usage?.total_tokens &&
-    response.usage?.total_tokens < MAX_TOKENS
-  ) {
-    deleteOlderMessages();
-  }
-
-  console.log(`${responseRole}: ${responseMessage}`);
 }
 
 process.stdin.addListener("data", async function (input) {
@@ -87,35 +66,3 @@ process.stdin.addListener("data", async function (input) {
 
   await createChatCompletion();
 });
-
-function deleteOlderMessages() {
-  let contestLength = getContextLength();
-  while (contestLength > MAX_TOKENS) {
-    for (let i = 0; i < context.length; i++) {
-      const message = context[i];
-      if (message.role !== "system") {
-        context.splice(i, 1);
-        contestLength = getContextLength();
-        console.log("New context length: " + contestLength);
-        break;
-      }
-    }
-  }
-}
-
-function getContextLength() {
-  let length = 0;
-
-  context.forEach(message => {
-    if (typeof message.content === "string") {
-      length += encoder.encode(message.content).length;
-    } else if (Array.isArray(message.content)) {
-      message.content.forEach(content => {
-        if (content.type === "text") {
-          length += encoder.encode(content.text).length;
-        }
-      });
-    }
-  });
-  return length;
-}
