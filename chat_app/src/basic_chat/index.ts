ee17dfbd55ccddb1535a37b038a1afa11bca1319
Roy Lopez <ryhsu2@gmail.com>
2024-09-15 15:29:58 +0200

    Update package.json and package-lock.json to include tiktoken dependency

--- chat_app/src/basic_chat/index.ts
+++ chat_app/src/basic_chat/index.ts
@@ -1,22 +1,82 @@
 import OpenAI from "openai";
+import { encoding_for_model } from "tiktoken";
+
 const openai = new OpenAI();
+const encoder = encoding_for_model("gpt-4o-mini");
+
+const MAX_TOKENS = 700;
+
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+  {
+    role: "system",
+    content: "You are an expert in Spain travel",
+  },
+];
+
+async function createChatCompletion() {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: context,
+    n: 1,
+  });
+
+  const responseRole = response.choices[0].message.role;
+  const responseMessage = response.choices[0].message.content;
+
+  context.push({
+    role: "assistant",
+    content: responseMessage,
+  });
+
+  if (
+    response.usage?.total_tokens &&
+    response.usage?.total_tokens < MAX_TOKENS
+  ) {
+    deleteOlderMessages();
+  }
+
+  console.log(`${responseRole}: ${responseMessage}`);
+}
 
 process.stdin.addListener("data", async function (input) {
   const userInput = input.toString().trim();
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      {
-        role: "system",
-        content: "You are an expert in Spain travel",
-      },
-      {
-        role: "user",
-        content: userInput,
-      },
-    ],
-    max_tokens: 150,
-    n: 1,
+
+  context.push({
+    role: "user",
+    content: userInput,
   });
-  console.log(response.choices[0].message.content, "\n");
+
+  await createChatCompletion();
 });
+
+function deleteOlderMessages() {
+  let contestLength = getContextLength();
+  while (contestLength > MAX_TOKENS) {
+    for (let i = 0; i < context.length; i++) {
+      const message = context[i];
+      if (message.role !== "system") {
+        context.splice(i, 1);
+        contestLength = getContextLength();
+        console.log("New context length: " + contestLength);
+        break;
+      }
+    }
+  }
+}
+
+function getContextLength() {
+  let length = 0;
+
+  context.forEach(message => {
+    if (typeof message.content === "string") {
+      length += encoder.encode(message.content).length;
+    } else if (Array.isArray(message.content)) {
+      message.content.forEach(content => {
+        if (content.type === "text") {
+          length += encoder.encode(content.text).length;
+        }
+      });
+    }
+  });
+  return length;
+}
